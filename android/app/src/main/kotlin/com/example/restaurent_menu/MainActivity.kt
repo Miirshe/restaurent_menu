9d0cc298952ff2486801394385ed38a3abba6212
Miirshe <miirshe@gmail.com>
2023-10-14 10:34:46 +0300

    updated some file

---
+++ android/app/src/main/kotlin/com/example/restaurent_menu/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.restaurent_menu
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/widgets/constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const bgColor = Colors.orange;
+const textColor = Colors.white;
